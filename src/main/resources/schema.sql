a3e67086eed5459432260764d01444a9e88aa10e
Jakub <jakub.kanclerz@gmail.com>
2019-12-04 09:25:51 +0100

    stora and load in sql db

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,11 @@
+
+
+ CREATE TABLE `cards` (
+    `number` VARCHAR(50) PRIMARY KEY,
+    `limit` DECIMAL(13,2) NOT NULL,
+    `balance` DECIMAL(13,2) NOT NULL
+ );
+
+
+
+
--- src/test/java/pl/krakow/uek/pp5/creditcard/model/SqlCreditCardStorageTest.java
+++ src/test/java/pl/krakow/uek/pp5/creditcard/model/SqlCreditCardStorageTest.java
@@ -2,20 +2,38 @@ package pl.krakow.uek.pp5.creditcard.model;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@SpringBootTest
+@RunWith(SpringRunner.class)
 public class SqlCreditCardStorageTest {
+
+    public static final String CREDIT_CARD_NUMBER = "1234";
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @Test
     public void itAllowToStoreCC() {
-        SqlCreditCardStorage sqlCreditCardStorage = new SqlCreditCardStorage();
+        SqlCreditCardStorage sqlStore = new SqlCreditCardStorage(jdbcTemplate);
 
-        CreditCard creditCard = new CreditCard("1234");
-        sqlCreditCardStorage.add(creditCard);
+        sqlStore.add(getCreditCard());
 
-        CreditCard loaded = sqlCreditCardStorage.load("1234");
+        CreditCard loaded = sqlStore.load(CREDIT_CARD_NUMBER);
 
-        assertThat(creditCard.getNumber()).isEqualTo(loaded.getNumber());
+        assertThat(CREDIT_CARD_NUMBER).isEqualTo(loaded.getNumber());
+    }
+
+    private CreditCard getCreditCard() {
+        CreditCard creditCard = new CreditCard(CREDIT_CARD_NUMBER);
+        creditCard.assignLimit(BigDecimal.valueOf(1000));
+        return creditCard;
     }
 }
